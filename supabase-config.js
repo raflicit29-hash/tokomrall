8107820f6f2ed12b4159547841bcc5041c52f0fe
raflicit29-hash <raflicit29@gmail.com>
2026-09-11 10:53:06 +0700

    Create supabase-config.js

---
+++ supabase-config.js
@@ -0,0 +1,8 @@
+const SUPABASE_URL = "https://naswzkxxzyadrccdvbeu.supabase.co";
+
+const SUPABASE_ANON_KEY = "sb_publishable_V6KpVrh7NfJaDAEGCgNneQ_UBAoUZkb";
+
+const supabaseClient = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
+);
